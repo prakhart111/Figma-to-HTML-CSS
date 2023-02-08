541c13b42401c317d448953133e0352542aff4ea
Prakhar <ptofficial29@gmail.com>
2023-02-08 17:13:54 +0530

    genrates html and css code

--- code.ts
+++ code.ts
@@ -1,35 +1,48 @@
 function generateCSS(className:string,node:any) {
   let cssTemp:string = "";
 
+  console.log(node);
+
   cssTemp+= `.${className}{`
+
+  cssTemp += `position: absolute;\n`;
+  cssTemp += `left: ${node.x}px;\n`;
+  cssTemp += `top: ${node.y}px;\n`;
   //background color
-  if (node.fills) {
+  if (node.fills[0]?.type==="SOLID") {
     let r:number = Math.floor(node.fills[0].color.r * 255)
     let g:number = Math.floor(node.fills[0].color.g * 255)
     let b:number = Math.floor(node.fills[0].color.b * 255)
     cssTemp += `background-color: rgb(${r},${g},${b});\n`;
   }
-  
+
+  // if(node.fills[0]?.type == "IMAGE"){
+  //   
+  // }
+
   //width and height
   if (node.width && node.height) {
     cssTemp += `width: ${node.width}px;\n`;
     cssTemp += `height: ${node.height}px;\n`;
   }
   
-  //position
-  if (node) {
-    cssTemp += `position: absolute;\n`;
-    cssTemp += `left: ${node.x}px;\n`;
-    cssTemp += `top: ${node.y}px;\n`;
+  //opacity
+  cssTemp += `opacity:${node.opacity ? node.opacity : 1};\n`
+
+  //border radius
+  if(typeof node?.cornerRadius === 'number'){
+    cssTemp += `border-radius:${node.opacity ? node.opacity : 1}px;\n`
   }
+
   cssTemp+= `}`
   return cssTemp;
 }
 
+
 async function exportFrame() {
   const frames = figma.currentPage.selection;
   if (frames.length !== 1 || frames[0].type !== "FRAME") {
-    figma.closePlugin("Please select a single Frame and try again.");
+    figma.notify("Please select a single Frame and try again.");
     return;
   }
 
@@ -43,8 +56,7 @@ async function exportFrame() {
 
   let index:number=0;
   for (const node of frame.children) {
-    console.log(`${node.name}-${node.id}`);
-    let nodeClassName = `child-${index}-${node.name.split(" ")?.[0]}-class}`
+    let nodeClassName = `child-${index}-${node.name.split(" ")?.[0]}-class`
     console.log(node.id);
 
     // html
@@ -61,5 +73,13 @@ async function exportFrame() {
   console.log(css);
 }
 
-exportFrame()
-figma.closePlugin()
+
+figma.showUI(__html__, { width: 300, height: 200, title:'HTML-CSS Zip Exporter' });
+figma.ui.onmessage = async msg => {
+  if (msg === "export") {
+    exportFrame();
+  }
+  if (msg === "close") {
+    figma.closePlugin();
+  }
+};
