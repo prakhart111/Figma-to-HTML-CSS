3474759087482d237680d8c0b8e1eb2e93b119a0
Prakhar <ptofficial29@gmail.com>
2024-03-30 23:01:03 +0530

    detection succesful

--- code.ts
+++ code.ts
@@ -1,23 +1,23 @@
-function generateCSS(className:string,node:any) {
-  let cssTemp:string = "";
+function generateCSS(className: string, node: any) {
+  let cssTemp: string = "";
 
   console.log(node);
 
-  cssTemp+= `.${className}{`
+  cssTemp += `.${className}{`;
 
   cssTemp += `position: absolute;\n`;
   cssTemp += `left: ${node.x}px;\n`;
   cssTemp += `top: ${node.y}px;\n`;
   //background color
-  if (node.fills[0]?.type==="SOLID") {
-    let r:number = Math.floor(node.fills[0].color.r * 255)
-    let g:number = Math.floor(node.fills[0].color.g * 255)
-    let b:number = Math.floor(node.fills[0].color.b * 255)
+  if (node.fills[0]?.type === "SOLID") {
+    let r: number = Math.floor(node.fills[0].color.r * 255);
+    let g: number = Math.floor(node.fills[0].color.g * 255);
+    let b: number = Math.floor(node.fills[0].color.b * 255);
     cssTemp += `background-color: rgb(${r},${g},${b});\n`;
   }
 
   // if(node.fills[0]?.type == "IMAGE"){
-  //   
+  //
   // }
 
   //width and height
@@ -25,19 +25,18 @@ function generateCSS(className:string,node:any) {
     cssTemp += `width: ${node.width}px;\n`;
     cssTemp += `height: ${node.height}px;\n`;
   }
-  
+
   //opacity
-  cssTemp += `opacity:${node.opacity ? node.opacity : 1};\n`
+  cssTemp += `opacity:${node.opacity ? node.opacity : 1};\n`;
 
   //border radius
-  if(typeof node?.cornerRadius === 'number'){
-    cssTemp += `border-radius:${node.opacity ? node.opacity : 1}px;\n`
+  if (typeof node?.cornerRadius === "number") {
+    cssTemp += `border-radius:${node.opacity ? node.opacity : 1}px;\n`;
   }
 
-  cssTemp+= `}`
+  cssTemp += `}`;
   return cssTemp;
 }
-
 
 async function exportFrame() {
   const frames = figma.currentPage.selection;
@@ -47,35 +46,39 @@ async function exportFrame() {
   }
 
   const frame = frames[0];
-  let html:string = `<html><head>
+  let html: string = `<html><head>
   <title>${frame.name}</title>
   <link rel="stylesheet" href="./style.css">
   </head><body>`;
-  let css:string = "";
+  let css: string = "";
   console.log(frame.name);
 
-  let index:number=0;
+  let index: number = 0;
   for (const node of frame.children) {
-    let nodeClassName = `child-${index}-${node.name.split(" ")?.[0]}-class`
+    let nodeClassName = `child-${index}-${node.name.split(" ")?.[0]}-class`;
     console.log(node.id);
 
     // html
-    html += `<div class="${nodeClassName}"` +
-            ` style="width: ${node.width}px; height: ${node.height}px;` +
-            ` left: ${node.x}px; top: ${node.y}px;"></div>`;
+    html +=
+      `<div class="${nodeClassName}"` +
+      ` style="width: ${node.width}px; height: ${node.height}px;` +
+      ` left: ${node.x}px; top: ${node.y}px;"></div>`;
     //css
-    css += generateCSS(nodeClassName,node)
+    css += generateCSS(nodeClassName, node);
     index++;
   }
 
-  html+="</body></html>"
+  html += "</body></html>";
   console.log(html);
   console.log(css);
 }
 
-
-figma.showUI(__html__, { width: 300, height: 200, title:'HTML-CSS Zip Exporter' });
-figma.ui.onmessage = async msg => {
+figma.showUI(__html__, {
+  width: 300,
+  height: 200,
+  title: "HTML-CSS Zip Exporter",
+});
+figma.ui.onmessage = async (msg) => {
   if (msg === "export") {
     exportFrame();
   }
